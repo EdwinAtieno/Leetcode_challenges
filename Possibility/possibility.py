e342299bf7ea888ec3ee353e8eb3b543c66145a5
edwinatieno <edwinmuthee11@gmail.com>
2023-04-25 07:53:28 +0300

    feat(key): 3 digit key
    * add code that gets the correct digit key after several hints

---
+++ Possibility/possibility.py
@@ -0,0 +1,51 @@
+from typing import Tuple
+
+
+def check_key(key: str, hint: str) -> Tuple[int, int]:
+    correct_count = 0
+    wrong_count = 0
+    # check for correct digit and placement
+    if key[0] == hint[0]:
+        correct_count += 1
+    # check for correct digit but wrong placement
+    elif key[0] in hint:
+        wrong_count += 1
+    if key[1] == hint[1]:
+        correct_count += 1
+    elif key[1] in hint:
+        wrong_count += 1
+    if key[2] == hint[2]:
+        correct_count += 1
+    elif key[2] in hint:
+        wrong_count += 1
+    # return a tuple with the number of correct and wrong digits
+    return correct_count, wrong_count
+
+
+# list of hints and their corresponding keys
+hints = [
+    ("682", (1, 0)),
+    ("614", (0, 1)),
+    ("206", (0, 2)),
+    ("738", (0, 0)),
+    ("780", (0, 1)),
+]
+
+# generate all possible 3-digit keys
+keys = [
+    str(i) + str(j) + str(k)
+    for i in range(0, 10)
+    for j in range(0, 10)
+    for k in range(0, 10)
+]
+
+# iterate over each key and check if it matches all the hints
+for key in keys:
+    match = True
+    for hint in hints:
+        if check_key(key, hint[0]) != hint[1]:
+            match = False
+            break
+    if match:
+        print("The correct key is:", key)
+        break
