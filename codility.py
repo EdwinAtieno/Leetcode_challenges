6ff5ebcc920a70a0802d478e2574fb9ec1d96c99
edwinatieno <edwinmuthee11@gmail.com>
2023-08-31 16:08:52 +0300

    new updates

--- codility.py
+++ codility.py
@@ -13,6 +13,7 @@ wo single rooms, and the other guests in another room. Write an efficient algori
  • N is an integer within the range [1..100,000]; • each element of array A is an integer within the range [1..100,000].
 
 """
+from typing import Any
 
 
 def room(A: list) -> int:
@@ -41,3 +42,52 @@ def room(A: list) -> int:
 
 
 print(room([7, 3, 1, 1, 4, 5, 4, 9]))
+
+
+"""
+Please help solve this python question A DOMINO is a rectangular title divide into two separate squares.
+Each square is decorated with a number of spots (also called pips) denoted with a value from 0 to 6.
+other is an array a of length 2n representing n dominoes. DOMINOES ARE ARRANGED IN A LINE IN THE ORDER in which
+they appear in array. the number of dots on the left and right parts of the k-th domino are A[2K] AND A[2*K+1]
+respectively. FOR EXAMPLE, AN ARRAY A=[2,4,1,3,4,6,2,4,1,6] REPRESENTS A SEQUENCE OF FIVE DOMINO TILES: (2,4) (1,3)
+(4,6) (2,4) (1,6). IN A CORRECT DOMINO sequence, each pair neighboring tiles should have the same number of dots on t
+heir adjacent parts. for example tiles (2,4) and (4,6) form a correct domino sequence and tiles (2,4) and (1,3) do not.
+ what is the minimum number of domino tiles that must be removed from the sequence so that the remaining tiles form a
+ correct domino sequence? example A=[2, 4, 1, 3, 4, 6, 2, 4, 1, 6] function returns 3
+"""
+
+
+def soliton(A: list) -> Any:
+    """# Initialize variables to keep track of the minimum number of domino tiles that must be removed
+    min_tiles = 0
+    k = 0
+
+    # Loop through the array A and check if the current pair of tiles form a correct domino sequence
+    while k < len(A) - 1:
+        # Get the number of dots on the left and right parts of the current pair of tiles
+        left = A[k]
+        right = A[k + 1]
+
+        # If the current pair of tiles do not form a correct domino sequence, remove one of the tiles
+        if left != right:
+            min_tiles += 1
+            k += 1
+
+        # Move on to the next pair of tiles
+        k += 1
+
+    # Return the minimum number of domino tiles that must be removed
+    return min_tiles"""
+
+    n = len(A) // 2
+    T = [[float("inf") for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        T[i][i] = 0
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            for k in range(i, j):
+                T[i][j] = min(T[i][j], T[i][k] + T[k + 1][j])
+    return T[1][n - 1]
+
+
+print(soliton([2, 4, 1, 3, 4, 6, 2, 4, 1, 6]))
